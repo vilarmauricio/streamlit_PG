e9a1cd877ce781795196a77803f9e0c338a84e2c
vilarmauricio <101902643+vilarmauricio@users.noreply.github.com>
2022-09-06 17:51:10 -0300

    se elimino grafico indicador

--- pages/temperatura.py
+++ pages/temperatura.py
@@ -115,66 +115,11 @@ def main():
      col1, col2, col3 = st.columns(3)
 
      with col1:
-            plot_bgcolor = "#def"              #verde claro, amarillo, naranja, rojo, verde oscuro
-            quadrant_colors = [plot_bgcolor, "#f25829",    "#f2a529", "#eff229","#85e043" ,"#2bad4e"]
-            quadrant_text = ["", "<b>Very high</b>", "<b>High</b>", "<b>Medium</b>", "<b>Low</b>", "<b>Very low</b>"]
-            n_quadrants = len(quadrant_colors) - 1
-
-            current_value = tabla_g['temperatura'].values[-1]
-            min_value = media_siglo_XX
-            max_value = temperatura_limite
-            hand_length = np.sqrt(2) / 4
-            hand_angle = np.pi * (1 - (max(min_value, min(max_value, current_value)) - min_value) / (max_value - min_value))
-
-            fig = go.Figure(
-                        data=[
-                            go.Pie(
-                            values=[0.5] + (np.ones(n_quadrants) / 2 / n_quadrants).tolist(),
-                            rotation=90,
-                            hole=0.5,
-                            marker_colors=quadrant_colors,
-                            # text = quadrant_text, aniade texto a los colores
-                            textinfo="text",
-                            hoverinfo="skip",
-                            ),
-                        ],
-            layout=go.Layout(
-                        showlegend=False,
-                        margin=dict(b=0,t=10,l=10,r=10),
-                        width=450,
-                        height=450,
-                        paper_bgcolor=plot_bgcolor,
-                        annotations=[
-                            go.layout.Annotation(
-                                    text=f"<b>Proporcion de Energias Renovables del Total Consumido:</b><br>{current_value} %",
-                                    x=0.5, xanchor="center", xref="paper",
-                                    y=0.25, yanchor="bottom", yref="paper",
-                                    showarrow=False,
-                            )
-                        ],
-            shapes=[
-                    go.layout.Shape(
-                            type="circle",
-                            x0=0.48, x1=0.52,
-                            y0=0.48, y1=0.52,
-                            fillcolor="#333",
-                            line_color="#333",
-                        ),
-                    go.layout.Shape(
-                            type="line",
-                            x0=0.5, x1=0.5 + hand_length * np.cos(hand_angle),
-                            y0=0.5, y1=0.5 + hand_length * np.sin(hand_angle),
-                            line=dict(color="#333", width=4)
-                        )
-                    ]
-            )
-        )
-            fig.show()
-            #indicador1 = graficos.indicador_kpi_temp(tabla_g, 'temperatura', temperatura_limite)
-            #indicador1.show()
-            #st.header("KPI's")
-            #st.title("+ " + str(round((media_actual - media_siglo_XX), 2))+"°C")
-            #st.progress(round(kpi_estado))
+           
+            
+            st.header("KPI's")
+            st.title("+ " + str(round((media_actual - media_siglo_XX), 2))+"°C")
+            st.progress(round(kpi_estado))
             
 
      with col2:
